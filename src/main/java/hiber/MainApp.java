f5eb56adda1981a33a800b6acdb4f6eea4642f2c
Suprimi4 <1LoveTwitch1@gmail.com>
2025-02-27 01:04:03 +0300

    init commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,23 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user = userService.getUserWithCar("BMW",330);
+        System.out.println(user.toString());
+
+        context.close();
+    }
+}
