7cf635263ce244f7b174ef532d532746d28eb153
Suprimi4 <1LoveTwitch1@gmail.com>
2025-02-28 17:53:43 +0300

    task222 fix

--- src/main/java/com/suprimi4/boot/service/CarServiceImpl.java
+++ src/main/java/com/suprimi4/boot/service/CarServiceImpl.java
@@ -2,11 +2,6 @@ package com.suprimi4.boot.service;
 
 import com.suprimi4.boot.model.Car;
 import com.suprimi4.boot.repository.CarRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.ListPagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
