7fd965dfb078da560f6f4a7361f226a5f037c3d6
Suprimi4 <1LoveTwitch1@gmail.com>
2025-03-04 02:21:17 +0300

    add Lombock

--- src/main/java/com/suprimi4/boot/model/Car.java
+++ src/main/java/com/suprimi4/boot/model/Car.java
@@ -1,63 +1,26 @@
 package com.suprimi4.boot.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
 @Table(name = "cars")
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String mark;
-    private String model;
-    private int yearOfManufacture;
+    private final String mark;
+    private final String model;
+    private final int yearOfManufacture;
 
-    public Car() {
-    }
 
-    public Car(int id, String mark, String model, int yearOfManufacture) {
-        this.id = id;
-        this.mark = mark;
-        this.model = model;
-        this.yearOfManufacture = yearOfManufacture;
-    }
 
-    public Car(String mark, String model, int yearOfManufacture) {
-        this.mark = mark;
-        this.model = model;
-        this.yearOfManufacture = yearOfManufacture;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMark() {
-        return mark;
-    }
-
-    public void setMark(String mark) {
-        this.mark = mark;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getYearOfManufacture() {
-        return yearOfManufacture;
-    }
-
-    public void setYearOfManufacture(int yearOfManufacture) {
-        this.yearOfManufacture = yearOfManufacture;
-    }
 }
